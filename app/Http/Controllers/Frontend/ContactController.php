f2eee2bd90c0235b61537ebacdc4daa74abed27d
Arindam Dasgupta <arindam@cybergys.com>
2023-04-13 16:50:08 +0530

    Contact Update

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -8,23 +8,24 @@ use App\Models\Admin\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Validation\Validator;
 
 class ContactController extends Controller
 {
-    public function contact(){
+    public function contact()
+    {
         $contact_data = Page::where('name', 'contact')->first();
         return view('frontend.contact', compact('contact_data'));
     }
 
-    public function email(Request $request){
+    public function email(Request $request)
+    {
         $validator = \Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email',
             'message' => 'required'
         ]);
 
-        if(!$validator->passes()){
+        if (!$validator->passes()) {
             return response()->json([
                 'code' => 0,
                 'error_message' => $validator->errors()->toArray()
@@ -32,9 +33,9 @@ class ContactController extends Controller
         } else {
             $subject = 'Contact form submission';
             $message = 'Visitor email information: <br/><br/>';
-            $message.= 'Name: ' . $request->name . '<br/>';
-            $message.= 'Email: ' . $request->email . '<br/>';
-            $message.= 'Message: ' . $request->message . '<br/>';
+            $message .= 'Name: ' . $request->name . '<br/>';
+            $message .= 'Email: ' . $request->email . '<br/>';
+            $message .= 'Message: ' . $request->message . '<br/>';
 
             $admin_data = Admin::where('id', 1)->first();
             $admin_email = $admin_data->email;
